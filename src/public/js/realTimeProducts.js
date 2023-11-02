8170dd9a8c0cd5e76bd38c6d28692dabb341b6f3
mauperez9918 <mauperez9918@gmail.com>
2023-11-02 00:03:06 -0300

    DeleteProduct socket

--- src/public/js/realTimeProducts.js
+++ src/public/js/realTimeProducts.js
@@ -2,16 +2,18 @@ const socket = io();
 
 socket.on("getProducts", (products) => {
   let productCard = document.getElementById("productCard");
-
+  productCard.innerHTML = "";
   products.forEach((product) => {
     productCard.innerHTML += `
     <p>${product.id}</p> <p>${product.title}</p>`;
   });
 });
 
-let deleteForm = document.getElementsByClassName("deleteForm");
-deleteForm.innerHTML = `<button class="btnDelete" data-id=${product.id}>Eliminar Producto</button>`;
-let button = document.getElementsByClassName("btnDelete");
-button.addEventListener("click", () => {
-  socket.emit("deleteProduct", boton.dataset.id);
+const deleteForm = document.getElementById("deleteForm");
+const input = document.getElementById("inputId");
+
+deleteForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  input.value = "";
+  socket.emit("deleteProduct", input.value);
 });
